7307c4a90e351098a319543c3f58433a0b88d5d4
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-06-21 22:51:39 -0400

    Update ParentMainView.java

--- Final Project/com/culminating/ui/ParentMainView.java
+++ Final Project/com/culminating/ui/ParentMainView.java
@@ -14,49 +14,49 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ParentMainView extends Application {
-	/**
-	 * Current login user
-	 */
-	private User currentUser;
+   /**
+    * Current login user
+    */
+   private User currentUser;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Library System");
-		FXMLLoader loader = new FXMLLoader();
-		if (currentUser.getType().equals("Librarian")) {
-			loader.setLocation(getClass().getResource("StaffParentMain.fxml")); //show staff UI
-		} else {
-			loader.setLocation(getClass().getResource("UserParentMain.fxml")); //show borrower UI
-		}
+   @Override
+   public void start(Stage primaryStage) throws Exception {
+   	primaryStage.setTitle("Library System");
+   	FXMLLoader loader = new FXMLLoader();
+   	if (currentUser.getType().equals("Librarian")) {
+   	   loader.setLocation(getClass().getResource("StaffParentMain.fxml")); //show staff UI
+   	} else {
+   	   loader.setLocation(getClass().getResource("UserParentMain.fxml")); //show borrower UI
+   	}
 
-		Pane myPane = loader.load();
+   	Pane myPane = loader.load();
 
-		// access the controller and set currentUser to the login user
-		if (currentUser.getType().equals("Librarian")) {
-			StaffParentMainController mainWindowController = loader.getController();
-			mainWindowController.initData(currentUser);
-		} else {
-			UserParentMainController mainWindowController = loader.getController();
-			mainWindowController.initData(currentUser);
-		}
+   	// access the controller and set currentUser to the login user
+   	if (currentUser.getType().equals("Librarian")) {
+   	   StaffParentMainController mainWindowController = loader.getController();
+   	   mainWindowController.initData(currentUser);
+   	} else {
+   	   UserParentMainController mainWindowController = loader.getController();
+   	   mainWindowController.initData(currentUser);
+   	}
 
-		Scene myScene = new Scene(myPane);
-		myScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(myScene);
-	}
+   	Scene myScene = new Scene(myPane);
+   	myScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+   	primaryStage.setScene(myScene);
+   }
 
-	/**
-	 * get current login user.
-	 */
-	public User getCurrentUser() {
-		return currentUser;
-	}
+   /**
+    * get current login user.
+    */
+   public User getCurrentUser() {
+   	return currentUser;
+   }
 
-	/**
-	 * set current login user
-	 * @param currentUser, the login user.
-	 */
-	public void setCurrentUser(User currentUser) {
-		this.currentUser = currentUser;
-	}
+   /**
+    * set current login user
+    * @param currentUser, the login user.
+    */
+   public void setCurrentUser(User currentUser) {
+   	this.currentUser = currentUser;
+   }
 }
