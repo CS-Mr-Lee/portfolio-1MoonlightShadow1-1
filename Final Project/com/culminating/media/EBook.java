0ccc8721af987d290afa88a92489ae8b297ffad6
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-06-21 21:50:06 -0400

    Update EBook.java

--- Final Project/com/culminating/media/EBook.java
+++ Final Project/com/culminating/media/EBook.java
@@ -1,7 +1,7 @@
 /**
-*Name(s): Franklin, Mike, Grace, Sophia
-*Date: 2022-05-04
-*Description: E-book class
+* Name(s): Franklin, Mike, Grace, Sophia
+* Date: 2022-05-04
+* Description: E-book class
 */
 
 package com.culminating.media;
@@ -18,16 +18,16 @@ public class EBook extends Book {
     }
 
     /**
-     *Constructor an EBook: sets publishDate, author, SIN, language, publisher, ageRating, genre, pages, and isbn from parameters
-     *@param publishDate, the publish date of the EBook
-     *@param author, the name of the author who wrote the EBook
-     *@param SIN, the unique identity of the EBook
-     *@param language, the language the EBook is written in
-     *@param publisher, the Ebook's publisher's name
-     *@param ageRating, the age rating of the EBook
-     *@param genre, the genre of the EBook
-     *@param pages, the number of pages the EBook has
-     *@param isbn, the thirteen digit code of the EBook 
+     * Constructor an EBook: sets publishDate, author, SIN, language, publisher, ageRating, genre, pages, and isbn from parameters
+     * @param publishDate, the publish date of the EBook
+     * @param author, the name of the author who wrote the EBook
+     * @param SIN, the unique identity of the EBook
+     * @param language, the language the EBook is written in
+     * @param publisher, the Ebook's publisher's name
+     * @param ageRating, the age rating of the EBook
+     * @param genre, the genre of the EBook
+     * @param pages, the number of pages the EBook has
+     * @param isbn, the thirteen digit code of the EBook 
      */
     public EBook(String name, LocalDate publishDate, String author, int SIN, String language,
             String publisher, Character ageRating, String genre, int pages, int isbn, int checkOutNumber, int totalNumber, String imagePath, String description) {
@@ -37,7 +37,8 @@ public class EBook extends Book {
     
         
     /**
-     *Description: prints all the attributes of the EBook
+     * Description: prints all the attributes of the EBook
+     * @return String all the attributes of the EBook
      */
     @Override
     public String toString() {
