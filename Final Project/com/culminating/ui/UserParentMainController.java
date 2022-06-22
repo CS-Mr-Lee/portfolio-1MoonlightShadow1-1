06892d1607c5404bf9aebdc4eded0228baa0ea8c
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-06-21 23:12:46 -0400

    Update UserParentMainController.java

--- Final Project/com/culminating/ui/UserParentMainController.java
+++ Final Project/com/culminating/ui/UserParentMainController.java
@@ -35,146 +35,146 @@ import javafx.scene.paint.Color;
 
 public class UserParentMainController implements Initializable {
 
-	private final Background YellowBackground = new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY,Insets.EMPTY));
+   private final Background YellowBackground = new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY,Insets.EMPTY));
                
-	private final Background blueBackground = new Background(new BackgroundFill(Color.web("#42d1f5"), CornerRadii.EMPTY,Insets.EMPTY));
+   private final Background blueBackground = new Background(new BackgroundFill(Color.web("#42d1f5"), CornerRadii.EMPTY,Insets.EMPTY));
 
-	@FXML
-	GridPane logoPane;
-	
-	@FXML
-	BorderPane parentPane;
-
-	@FXML
-	private Label userName;
-
-	private User currentUser;
-	@FXML
-	private VBox parentLeftVbox;
-
-	@FXML
-	private HBox topHbox;
-
-	@FXML
-	private ImageView logo;
-	
-	@FXML
-	private ImageView mainPageImg;
-
-	@FXML
-	private Button btnCheckOut;
-	
-	@FXML
-	private Button btnHold;
-	
-	@FXML
-	private Button btnMedia;
-	
-	@FXML
-	private Button btnSwithUser;
-
-	/**
-	 * Initializes the controller class. This method is automatically called
-	 * after the fxml file has been loaded.
-	 */
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		parentLeftVbox.setBackground(YellowBackground);
-		logoPane.setBackground(blueBackground);
-	}
-
-	/**
-	 * set the current user to login user and load the backgroud image.
-	 * @param user, the current login user.
-	 */
-	public void initData(User user) {
-		this.currentUser = user;
-		userName.setText("Welcome " + user.getName());
-		FileInputStream input;
-		try {
-			input = new FileInputStream("com//culminating//ui//schoolbanner.jpg");
-			Image image = new Image(input);
-			logo.setImage(image);
-			logo.fitWidthProperty().bind(logoPane.widthProperty());
-			logo.setPreserveRatio(false);
-			
-			FileInputStream input2 = new FileInputStream("com//culminating//ui//mainpage.jpg");
-			Image image2 = new Image(input2);
-			mainPageImg.setImage(image2);
-			mainPageImg.setPreserveRatio(true);
-			parentPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * show all checkout medias action.
-	 * @param event, the ActionEvent.
-	 */
-	@FXML
-	private void checkOut(ActionEvent event) {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("UserCheckOutsMedia.fxml"));
-		try {
-			Pane myPane = loader.load();
-			UserCheckOutsMediaController mainWindowController = loader.getController();
-			mainWindowController.initData(currentUser);
-			parentPane.setCenter(myPane);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+   @FXML
+   GridPane logoPane;
    
-	/**
-	 * user relogin action.
-	 * @param event, the ActionEvent.
-	 */
-	@FXML
-	private void swithUser(ActionEvent event) {
-		try {
-   		FXMLLoader loader = new FXMLLoader();
-   		loader.setLocation(getClass().getResource("Login.fxml"));
-			Pane myPane = loader.load();
-			parentPane.setCenter(myPane);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+   @FXML
+   BorderPane parentPane;
+
+   @FXML
+   private Label userName;
+
+   private User currentUser;
+   @FXML
+   private VBox parentLeftVbox;
+
+   @FXML
+   private HBox topHbox;
+
+   @FXML
+   private ImageView logo;
    
-	/**
-	 * show all hold medias action.
-	 * @param event, the ActionEvent.
-	 */
-	@FXML
-	private void hold(ActionEvent event) {
-		try {
-	   	FXMLLoader loader = new FXMLLoader();
-	   	loader.setLocation(getClass().getResource("UserHoldMedia.fxml"));
-			Pane myPane = loader.load();
-			UserHoldMediaController mainWindowController = loader.getController();
-			mainWindowController.initData(currentUser);
-			parentPane.setCenter(myPane);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+   @FXML
+   private ImageView mainPageImg;
+
+   @FXML
+   private Button btnCheckOut;
    
-	/**
-	 * show all medias action.
-	 * @param event, the ActionEvent.
-	 */
-	@FXML
-	private void showMedia(ActionEvent event) {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("UserBorrowMedia.fxml"));
-		try {
-			Pane myPane = loader.load();
-			UserBorrowMediaController mainWindowController = loader.getController();
-			mainWindowController.initData(currentUser);
-			parentPane.setCenter(myPane);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+   @FXML
+   private Button btnHold;
+   
+   @FXML
+   private Button btnMedia;
+   
+   @FXML
+   private Button btnSwithUser;
+
+   /**
+    * Initializes the controller class. This method is automatically called
+    * after the fxml file has been loaded.
+    */
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+   	parentLeftVbox.setBackground(YellowBackground);
+   	logoPane.setBackground(blueBackground);
+   }
+
+   /**
+    * set the current user to login user and load the backgroud image.
+    * @param user, the current login user.
+    */
+   public void initData(User user) {
+   	this.currentUser = user;
+   	userName.setText("Welcome " + user.getName());
+   	FileInputStream input;
+   	try {
+   	   input = new FileInputStream("com//culminating//ui//schoolbanner.jpg");
+   	   Image image = new Image(input);
+   	   logo.setImage(image);
+   	   logo.fitWidthProperty().bind(logoPane.widthProperty());
+   	   logo.setPreserveRatio(false);
+   	   
+   	   FileInputStream input2 = new FileInputStream("com//culminating//ui//mainpage.jpg");
+   	   Image image2 = new Image(input2);
+   	   mainPageImg.setImage(image2);
+   	   mainPageImg.setPreserveRatio(true);
+   	   parentPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+   	} catch (FileNotFoundException e) {
+   	   e.printStackTrace();
+   	}
+   }
+
+   /**
+    * show all checkout medias action.
+    * @param event, the ActionEvent.
+    */
+   @FXML
+   private void checkOut(ActionEvent event) {
+   	FXMLLoader loader = new FXMLLoader();
+   	loader.setLocation(getClass().getResource("UserCheckOutsMedia.fxml"));
+   	try {
+   	   Pane myPane = loader.load();
+   	   UserCheckOutsMediaController mainWindowController = loader.getController();
+   	   mainWindowController.initData(currentUser);
+   	   parentPane.setCenter(myPane);
+   	} catch (IOException e) {
+   	   e.printStackTrace();
+   	}
+   }
+   
+   /**
+    * user relogin action.
+    * @param event, the ActionEvent.
+    */
+   @FXML
+   private void swithUser(ActionEvent event) {
+   	try {
+      	FXMLLoader loader = new FXMLLoader();
+      	loader.setLocation(getClass().getResource("Login.fxml"));
+   	   Pane myPane = loader.load();
+   	   parentPane.setCenter(myPane);
+   	} catch (IOException e) {
+   	   e.printStackTrace();
+   	}
+   }
+   
+   /**
+    * show all hold medias action.
+    * @param event, the ActionEvent.
+    */
+   @FXML
+   private void hold(ActionEvent event) {
+   	try {
+         FXMLLoader loader = new FXMLLoader();
+         loader.setLocation(getClass().getResource("UserHoldMedia.fxml"));
+   	   Pane myPane = loader.load();
+   	   UserHoldMediaController mainWindowController = loader.getController();
+   	   mainWindowController.initData(currentUser);
+   	   parentPane.setCenter(myPane);
+   	} catch (IOException e) {
+   	   e.printStackTrace();
+   	}
+   }
+   
+   /**
+    * show all medias action.
+    * @param event, the ActionEvent.
+    */
+   @FXML
+   private void showMedia(ActionEvent event) {
+   	FXMLLoader loader = new FXMLLoader();
+   	loader.setLocation(getClass().getResource("UserBorrowMedia.fxml"));
+   	try {
+   	   Pane myPane = loader.load();
+   	   UserBorrowMediaController mainWindowController = loader.getController();
+   	   mainWindowController.initData(currentUser);
+   	   parentPane.setCenter(myPane);
+   	} catch (IOException e) {
+   	   e.printStackTrace();
+   	}
+   }
 }
