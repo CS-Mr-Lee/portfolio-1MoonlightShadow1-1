9fab08ef4d4fca9d95b0abf2980dbd32758cf2e7
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-06-21 22:06:28 -0400

    Update VideoGames.java

--- Final Project/com/culminating/media/VideoGames.java
+++ Final Project/com/culminating/media/VideoGames.java
@@ -1,8 +1,8 @@
 /**
-*Name(s): Franklin, Mike, Grace, Sophia
-*Date: 2022-05-04
-*Description: Video games class
-*/
+ * Name(s): Franklin, Mike, Grace, Sophia
+ * Date: 2022-05-04
+ * Description: Video games class
+ */
 
 //packages video games in com.culminating.media
 package com.culminating.media;
@@ -13,15 +13,27 @@ import org.json.simple.JSONObject;
 import java.util.Date;
 
 public class VideoGames extends Media {
-    private String platform; //the platform of the video game
-    private String[] peripheralsRequired; //the peripheral required for the video game
-    private int amtPlayers; //the amount of players for the video game
+    
+    /**
+     * the platform of the video game
+     */
+    private String platform; 
+    
+    /**
+     * the peripheral required for the video game
+     */
+    private String[] peripheralsRequired; 
+    
+    /**
+     * the amount of players for the video game
+     */
+    private int amtPlayers;
 
     /**
-     *default constuctor a video game: call superclass media
-     *sets platform to ""
-     *sets amtPlayers to -1
-     *sets peripheralsRequired to new String[1]
+     * default constuctor a video game: call superclass media
+     * sets platform to ""
+     * sets amtPlayers to -1
+     * sets peripheralsRequired to new String[1]
      */
     public VideoGames() {
         this.platform = "";
@@ -31,17 +43,17 @@ public class VideoGames extends Media {
     }
 
     /**
-     *Constructor a video game: sets publishDate, author, SIN, language, publisher, ageRating, genre, platform, peripheralsRequired, and amtPlayers from parameters
-     *@param publishDate, the publication date of the game
-     *@param author, the video game's author(creator)'s name
-     *@param SIN, the unique identity of the video game
-     *@param language, the language the video game is in
-     *@param publisher, the publiser of the video game
-     *@param ageRating, the age rating of the video game
-     *@param genre, the genre of the video game
-     *@param platform, the platform of the video game
-     *@param peripheralsRequired, the peripherals required for the video game
-     *@param amtPlayerss, the amount of players the video game need
+     * Constructor a video game: sets publishDate, author, SIN, language, publisher, ageRating, genre, platform, peripheralsRequired, and amtPlayers from parameters
+     * @param publishDate, the publication date of the game
+     * @param author, the video game's author(creator)'s name
+     * @param SIN, the unique identity of the video game
+     * @param language, the language the video game is in
+     * @param publisher, the publiser of the video game
+     * @param ageRating, the age rating of the video game
+     * @param genre, the genre of the video game
+     * @param platform, the platform of the video game
+     * @param peripheralsRequired, the peripherals required for the video game
+     * @param amtPlayerss, the amount of players the video game need
      */
     public VideoGames(String name, LocalDate publishDate, String author, int SIN, String language,
             String publisher, Character ageRating, String genre, String platform,
@@ -54,9 +66,9 @@ public class VideoGames extends Media {
     }
     
     /**
-     *Description: Gets json object represent of this class object
+     * Description: Gets json object represent of this class object
      * 
-     *@return jsonobject
+     * @return jsonobject
      */
     public JSONObject getJSONObject() {
         JSONObject obj = super.getJSONObject();
@@ -72,55 +84,56 @@ public class VideoGames extends Media {
     }
         
     /**
-     *Description: gets the platform
-     *@return String platform of the video game
+     * Description: gets the platform
+     * @return String platform of the video game
      */
     public String getPlatform() {
         return this.platform;
     }
         
     /**
-     *Description: gets the peripherals required
-     *@return String[] peripheralsRequired of the video game
+     * Description: gets the peripherals required
+     * @return String[] peripheralsRequired of the video game
      */
     public String[] getPeripheralsRequired() {
         return this.peripheralsRequired;
     }
         
     /**
-     *Description: gets the amount of players
-     *@return int amtPlayers of the video game
+     * Description: gets the amount of players
+     * @return int amtPlayers of the video game
      */
     public int getAmtPlayers() {
         return this.amtPlayers;
     }
         
     /**
-     *Description: sets the platform
-     *@param platform, the video game's platform
+     * Description: sets the platform
+     * @param platform, the video game's platform
      */
     public void setPlatform(String platform) {
         this.platform = platform;
     }
         
     /**
-     *Description: sets the peripheral required
-     *@param peripheralsRequired, the peripherals required for the video game
+     * Description: sets the peripheral required
+     * @param peripheralsRequired, the peripherals required for the video game
      */
     public void setPeripheralsRequired(String[] peripheralsRequired) {
         this.peripheralsRequired = peripheralsRequired;
     }
         
     /**
-     *Description: sets the amount of players
-     *@param amtPlayers, the amount of players for the video game
+     * Description: sets the amount of players
+     * @param amtPlayers, the amount of players for the video game
      */
     public void setAmtPlayers(int amtPlayers) {
         this.amtPlayers = amtPlayers;
     }
 
     /**
-     *Description: prints all the attributes of the video game
+     * Description: prints all the attributes of the video game
+     * @return String all the attributes of the video game
      */
     @Override
     public String toString() {
