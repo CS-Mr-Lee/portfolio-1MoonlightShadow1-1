5661ce5c922cc9a90db334094e6e80c06b1cc509
1MoonlightShadow1 <91137694+1MoonlightShadow1@users.noreply.github.com>
2022-06-21 22:58:43 -0400

    Update StaffHoldMediaController.java

--- Final Project/com/culminating/ui/StaffHoldMediaController.java
+++ Final Project/com/culminating/ui/StaffHoldMediaController.java
@@ -32,206 +32,206 @@ import javafx.scene.image.ImageView;
 
 public class StaffHoldMediaController {
 
-	/**
-	 * Current login user
-	 */
-	private User currentUser;
+   /**
+    * Current login user
+    */
+   private User currentUser;
 
-	@FXML
-	private ImageView bookImage;
+   @FXML
+   private ImageView bookImage;
 
-	@FXML
-	private Button bthCheckOut;
+   @FXML
+   private Button bthCheckOut;
 
-	@FXML
-	private Button btnRemove;
+   @FXML
+   private Button btnRemove;
 
-	@FXML
-	private TextField filterField;
+   @FXML
+   private TextField filterField;
 
-	@FXML
-	private TextField publisherField;
+   @FXML
+   private TextField publisherField;
 
-	@FXML
-	private Label descriptionField;
-	@FXML
-	private TextField languageField;
-	@FXML
-	private TableView<ItemStatus> mediaTable;
-	@FXML
-	private TableColumn<ItemStatus, String> nameColumn;
-	@FXML
-	private TableColumn<ItemStatus, Boolean> statusColumn;
-	@FXML
-	private TableColumn<ItemStatus, String> authorColumn;
-	@FXML
-	private TableColumn<ItemStatus, String> typeColumn;
-	@FXML
-	private TableColumn<ItemStatus, Date> holdDateColumn;
-	@FXML
-	private TableColumn<ItemStatus, String> holdByColumn;
-	
-	/**
-	 * All hold medias by all users.
-	 */
-	private ObservableList<ItemStatus> holdMedias;
+   @FXML
+   private Label descriptionField;
+   @FXML
+   private TextField languageField;
+   @FXML
+   private TableView<ItemStatus> mediaTable;
+   @FXML
+   private TableColumn<ItemStatus, String> nameColumn;
+   @FXML
+   private TableColumn<ItemStatus, Boolean> statusColumn;
+   @FXML
+   private TableColumn<ItemStatus, String> authorColumn;
+   @FXML
+   private TableColumn<ItemStatus, String> typeColumn;
+   @FXML
+   private TableColumn<ItemStatus, Date> holdDateColumn;
+   @FXML
+   private TableColumn<ItemStatus, String> holdByColumn;
    
-	/**
-	 * The selected media when user click on the table.
-	 */
-	private ObservableList<ItemStatus> selectedItems;
-
-	/**
-	 * The hold media in sorted.
-	 */
-	private SortedList<ItemStatus> sortedData;
-
-	/**
-	 * The filter medias by user search.
-	 */
-	private FilteredList<ItemStatus> filteredData;
-
-	/**
-	 * initial the media table data.
-	 * Initializes the table columns and sets up sorting and filtering.
-	 * @param user, the current login user.
-	 */
-	public void initData(User user) {
-		this.currentUser = user;
-		ArrayList<ItemStatus> allHolds = Handler.sharedInstance().getCurrHolds();
-		holdMedias = FXCollections.observableArrayList(allHolds);
-		// 0. Initialize the columns.
-		nameColumn.setCellValueFactory(new PropertyValueFactory<>("mediaName"));
-
-		authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
-
-		typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
-
-		holdDateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-		statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
-		
-		holdByColumn.setCellValueFactory(new PropertyValueFactory<>("userName"));
-		// 1. Wrap the ObservableList in a FilteredList (initially display all data).
-		filteredData = new FilteredList<>(holdMedias, p -> true);
-		// filteredData.
-		// 2. Set the filter Predicate whenever the filter changes.
-		filterField.textProperty().addListener(
-			(observable, oldValue, newValue) -> {
-				filteredData.setPredicate(media -> {
-					// If filter text is empty, display all medias.
-					if (newValue == null || newValue.isEmpty()) {
-						return true;
-					}
-
-					// Compare name of every media with filter text.
-					String lowerCaseFilter = newValue.toLowerCase();
-
-					if (media.getMediaName().toLowerCase().contains(lowerCaseFilter)) {
-						return true; // Filter matches first name.
-					}
-					return false; // Does not match.
-				});
-		});
-
-		// 3. Wrap the FilteredList in a SortedList.
-		sortedData = new SortedList<>(filteredData);
-
-		// 4. Bind the SortedList comparator to the TableView comparator.
-		sortedData.comparatorProperty().bind(mediaTable.comparatorProperty());
-
-		// 5. Add sorted (and filtered) data to the table.
-		mediaTable.setItems(sortedData);
-
-		// Obtain TableView SelectionModel Instance
-		TableViewSelectionModel selectionModel = mediaTable.getSelectionModel();
-
-		// set selection mode to only 1 row
-		selectionModel.setSelectionMode(SelectionMode.SINGLE);
-
-		// Get Selected Rows
-		selectedItems = selectionModel.getSelectedItems();
-
-		// Listening for Selection Changes
-		selectedItems.addListener(new ListChangeListener<ItemStatus>() {
-			@Override
-			public void onChanged(Change<? extends ItemStatus> change) {
-				showMediaDetails();
-			}
-		});
-		bthCheckOut.setDisable(true);
-	}
-
-	/**
-	 * Initializes the controller class. This method is automatically called
-	 * after the fxml file has been loaded.
-	 */
-	@FXML
-	private void initialize() {
-
-	}
-
-	/**
-	 * Show media detail information when user click the mmedia on the table  
-	 */
-	private void showMediaDetails() {
-		if (selectedItems != null && selectedItems.get(0) != null) {
-			Image image = new Image(selectedItems.get(0).getItem().getImagePath());
-			bookImage.setImage(image);
-			bookImage.setPreserveRatio(false);
-
-			publisherField.setText(selectedItems.get(0).getItem().getPublisher());
-			publisherField.setEditable(false);
-			descriptionField.setText(selectedItems.get(0).getItem().getDescription());
-			descriptionField.setWrapText(true);
-			languageField.setText(selectedItems.get(0).getItem().getLanguage());
-			languageField.setEditable(false);
-			bthCheckOut.setDisable(!selectedItems.get(0).getStatus());
-		}
-	}
-
-	/**
-	 * remove media action.
-	 * @param event, the action event.
-	 */
-	@FXML
-	protected void removeMedia(ActionEvent event) {
-		if (selectedItems != null && selectedItems.get(0) != null) {
-			currentUser = selectedItems.get(0).getUser();
-			Handler.sharedInstance().removeHold(selectedItems.get(0).getItem(), currentUser);
-			// The index of the sorted and filtered list.
-			int visibleIndex = mediaTable.getSelectionModel().getSelectedIndex();
-
-			// Source index of master data.
-			int sourceIndex = sortedData.getSourceIndexFor(holdMedias, visibleIndex);
-
-			// Remove.
-			holdMedias.remove(sourceIndex);
-			mediaTable.refresh();
-		}
-	}
+   /**
+    * All hold medias by all users.
+    */
+   private ObservableList<ItemStatus> holdMedias;
    
-	/**
-	 * checkout media action.
-	 * @param event, the action event.
-	 */
-	@FXML
-	protected void checkout(ActionEvent event) {
-		if (selectedItems != null && selectedItems.get(0) != null && selectedItems.get(0).getStatus()) {
-			currentUser = selectedItems.get(0).getUser();
-			Handler.sharedInstance().removeHold(selectedItems.get(0).getItem(), currentUser);
-			
-			if (Handler.sharedInstance().checkOut(currentUser, selectedItems.get(0).getItem())) {
-				selectedItems.get(0).getItem().setCheckOutNumber(selectedItems.get(0).getItem().getCheckOutNumber() + 1);
-				// The index of the sorted and filtered list.
-				int visibleIndex = mediaTable.getSelectionModel().getSelectedIndex();
+   /**
+    * The selected media when user click on the table.
+    */
+   private ObservableList<ItemStatus> selectedItems;
 
-				// Source index of holdMedias.
-				int sourceIndex = sortedData.getSourceIndexFor(holdMedias, visibleIndex);
+   /**
+    * The hold media in sorted.
+    */
+   private SortedList<ItemStatus> sortedData;
 
-				// Remove media.
-				holdMedias.remove(sourceIndex);
-				mediaTable.refresh();
-			}			
-		}
-	}
+   /**
+    * The filter medias by user search.
+    */
+   private FilteredList<ItemStatus> filteredData;
+
+   /**
+    * initial the media table data.
+    * Initializes the table columns and sets up sorting and filtering.
+    * @param user, the current login user.
+    */
+   public void initData(User user) {
+   	this.currentUser = user;
+   	ArrayList<ItemStatus> allHolds = Handler.sharedInstance().getCurrHolds();
+   	holdMedias = FXCollections.observableArrayList(allHolds);
+   	// 0. Initialize the columns.
+   	nameColumn.setCellValueFactory(new PropertyValueFactory<>("mediaName"));
+
+   	authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+   	typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+
+   	holdDateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+   	statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+   	
+   	holdByColumn.setCellValueFactory(new PropertyValueFactory<>("userName"));
+   	// 1. Wrap the ObservableList in a FilteredList (initially display all data).
+   	filteredData = new FilteredList<>(holdMedias, p -> true);
+   	// filteredData.
+   	// 2. Set the filter Predicate whenever the filter changes.
+   	filterField.textProperty().addListener(
+   	   (observable, oldValue, newValue) -> {
+   	   	filteredData.setPredicate(media -> {
+   	   	   // If filter text is empty, display all medias.
+   	   	   if (newValue == null || newValue.isEmpty()) {
+   	   	   	return true;
+   	   	   }
+
+   	   	   // Compare name of every media with filter text.
+   	   	   String lowerCaseFilter = newValue.toLowerCase();
+
+   	   	   if (media.getMediaName().toLowerCase().contains(lowerCaseFilter)) {
+   	   	   	return true; // Filter matches first name.
+   	   	   }
+   	   	   return false; // Does not match.
+   	   	});
+   	});
+
+   	// 3. Wrap the FilteredList in a SortedList.
+   	sortedData = new SortedList<>(filteredData);
+
+   	// 4. Bind the SortedList comparator to the TableView comparator.
+   	sortedData.comparatorProperty().bind(mediaTable.comparatorProperty());
+
+   	// 5. Add sorted (and filtered) data to the table.
+   	mediaTable.setItems(sortedData);
+
+   	// Obtain TableView SelectionModel Instance
+   	TableViewSelectionModel selectionModel = mediaTable.getSelectionModel();
+
+   	// set selection mode to only 1 row
+   	selectionModel.setSelectionMode(SelectionMode.SINGLE);
+
+   	// Get Selected Rows
+   	selectedItems = selectionModel.getSelectedItems();
+
+   	// Listening for Selection Changes
+   	selectedItems.addListener(new ListChangeListener<ItemStatus>() {
+   	   @Override
+   	   public void onChanged(Change<? extends ItemStatus> change) {
+   	   	showMediaDetails();
+   	   }
+   	});
+   	bthCheckOut.setDisable(true);
+   }
+
+   /**
+    * Initializes the controller class. This method is automatically called
+    * after the fxml file has been loaded.
+    */
+   @FXML
+   private void initialize() {
+
+   }
+
+   /**
+    * Show media detail information when user click the mmedia on the table  
+    */
+   private void showMediaDetails() {
+   	if (selectedItems != null && selectedItems.get(0) != null) {
+   	   Image image = new Image(selectedItems.get(0).getItem().getImagePath());
+   	   bookImage.setImage(image);
+   	   bookImage.setPreserveRatio(false);
+
+   	   publisherField.setText(selectedItems.get(0).getItem().getPublisher());
+   	   publisherField.setEditable(false);
+   	   descriptionField.setText(selectedItems.get(0).getItem().getDescription());
+   	   descriptionField.setWrapText(true);
+   	   languageField.setText(selectedItems.get(0).getItem().getLanguage());
+   	   languageField.setEditable(false);
+   	   bthCheckOut.setDisable(!selectedItems.get(0).getStatus());
+   	}
+   }
+
+   /**
+    * remove media action.
+    * @param event, the action event.
+    */
+   @FXML
+   protected void removeMedia(ActionEvent event) {
+   	if (selectedItems != null && selectedItems.get(0) != null) {
+   	   currentUser = selectedItems.get(0).getUser();
+   	   Handler.sharedInstance().removeHold(selectedItems.get(0).getItem(), currentUser);
+   	   // The index of the sorted and filtered list.
+   	   int visibleIndex = mediaTable.getSelectionModel().getSelectedIndex();
+
+   	   // Source index of master data.
+   	   int sourceIndex = sortedData.getSourceIndexFor(holdMedias, visibleIndex);
+
+   	   // Remove.
+   	   holdMedias.remove(sourceIndex);
+   	   mediaTable.refresh();
+   	}
+   }
+   
+   /**
+    * checkout media action.
+    * @param event, the action event.
+    */
+   @FXML
+   protected void checkout(ActionEvent event) {
+   	if (selectedItems != null && selectedItems.get(0) != null && selectedItems.get(0).getStatus()) {
+   	   currentUser = selectedItems.get(0).getUser();
+   	   Handler.sharedInstance().removeHold(selectedItems.get(0).getItem(), currentUser);
+   	   
+   	   if (Handler.sharedInstance().checkOut(currentUser, selectedItems.get(0).getItem())) {
+   	   	selectedItems.get(0).getItem().setCheckOutNumber(selectedItems.get(0).getItem().getCheckOutNumber() + 1);
+   	   	// The index of the sorted and filtered list.
+   	   	int visibleIndex = mediaTable.getSelectionModel().getSelectedIndex();
+
+   	   	// Source index of holdMedias.
+   	   	int sourceIndex = sortedData.getSourceIndexFor(holdMedias, visibleIndex);
+
+   	   	// Remove media.
+   	   	holdMedias.remove(sourceIndex);
+   	   	mediaTable.refresh();
+   	   }   	   
+   	}
+   }
 }
